cafdeeb021c1430440f44d2964155b828620560b
“AlaaAlsubaie” <eng_a.a.a@hotmail.com>
2018-12-31 18:39:18 +0300

    RJS3-TheIndex-Components-and-Props

---
+++ src/AuthorList.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import AuthorCard from "./AuthorCard";
+
+class AuthorList extends Component{
+    render(){
+        let authorcard = this.props.authors.map(author => 
+            <AuthorCard author={author}  keys={author.last_name}/>);
+
+        return(
+            <div className="content col-10">
+            <div className="authors">
+                <h3>Authors</h3>
+                <div className="row">
+                   {authorcard}
+                </div>
+              </div>
+             </div>
+        
+        
+        );
+
+    }
+
+}
+
+export default AuthorList;
